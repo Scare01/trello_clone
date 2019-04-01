36ce4abfee38d19714c5704d9036645136197a26
Scare01 <lexsl84@gmail.com>
2019-04-01 15:28:57 +0700

    add redux action constants

---
+++ src/redux/constants/actionTypes.js
@@ -0,0 +1,9 @@
+// constants for board
+export const ADD_BOARD = "ADD_BOARD";
+export const DELETE_BOARD = "DELETE_BOARD";
+export const EDIT_BOARD = "EDIT_BOARD";
+
+// constants for tasks
+export const ADD_TASK = "ADD_TASK";
+export const DELETE_TASK = "DELETE_TASK";
+export const COMPLETE_TASK = "COMPLETE_TASK";
