01fa04d8f113564ebf4570e20224d69f4dc734b2
Scare01 <lexsl84@gmail.com>
2019-04-01 18:47:37 +0700

    add store to localStorage

---
+++ src/redux/store/localStorage.js
@@ -0,0 +1,20 @@
+export let loadStata = () => {
+  try {
+    let State = localStorage.getItem("boardState");
+    if (State === null) {
+      return undefined;
+    }
+    return JSON.parese(State);
+  } catch (err) {
+    return undefined;
+  }
+};
+
+export let saveState = state => {
+  try {
+    let State = JSON.stringify(state);
+    localStorage.setItem("boardState", State);
+  } catch (err) {
+    //ingonre err
+  }
+};
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,6 +1,15 @@
 import { createStore } from "redux";
 import rootReducer from "../reducers/index";
+import { loadState, saveState } from "./localStorage";
+import throttle from "lodash/throttle";
 
-let store = createStore(rootReducer);
+let State = loadState();
+let store = createStore(rootReducer, State);
+
+store.subscribe(
+  throttle(() => {
+    saveState({ boards: store.getState().boards });
+  }, 1000)
+);
 
 export default store;
